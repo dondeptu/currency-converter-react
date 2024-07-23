715e58aa502e04240d124c51682248c05f0440cc
Donata Deptula <dondeptu@gmail.com>
2024-07-23 15:54:56 +0200

    Result module simplified

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,19 +1,15 @@
 import "./style.css";
 
-const Result = ({ result }) => {
-    const resultToDisplay = +result;
-
-    return (
-        <p className="result">
-            Wynik:&nbsp;
-            {result !== undefined && (
-                <>
-                    <span className="result__outAmount">{resultToDisplay.toFixed(2)}</span>
-                    &nbsp;PLN
-                </>
-            )}
-        </p>
-    );
-};
+const Result = ({ result }) => (
+    <p className="result">
+        Wynik:&nbsp;
+        {result && (
+            <>
+                <span className="result__outAmount">{(+result).toFixed(2)}</span>
+                &nbsp;PLN
+            </>
+        )}
+    </p>
+);
 
 export default Result;
