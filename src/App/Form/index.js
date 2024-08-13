f3cae207e45166b91e8c2ec2b58ab90aa5807e8c
Donata Deptula <dondeptu@gmail.com>
2024-08-13 23:38:01 +0200

    application state management

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Result from "./Result";
-import { Button, Field, FieldSelect, Fieldset, Grid, Legend, Paragraph, StyledForm } from "./styled";
+import { Button, ErrorStatus, Field, FieldSelect, Fieldset, Grid, Legend, LoadingStatus, Paragraph, StyledForm } from "./styled";
 import { useRatesData } from "./useRatesData";
 import { RatesUpdatedTime } from "./RatesUpdatedTime";
+import { LoadingAnimation } from "./LoadingAnimation";
 
 const Form = () => {
-    const ratesData = useRatesData();
     const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
     const [result, setResult] = useState(null);
+    const ratesData = useRatesData();
 
     console.log(ratesData);
 
@@ -24,50 +25,61 @@ const Form = () => {
     };
 
     return (
-        <StyledForm onSubmit={onSubmit}>
-            <Fieldset>
-                <Legend>Zamień na złotówki:</Legend>
-                <Paragraph>
-                    <Grid>
-                        <span> Waluta: </span>
-                        <FieldSelect
-                            onChange={({ target }) => setCurrency(target.value)}
-                            name="convertFrom"
-                            value={currency}
-                        >
-                            {!!ratesData.rates && Object.keys(ratesData.rates).map(currency => (
-                                <option key={currency} value={currency}>
-                                    {currency}
-                                </option>
-                            ))}
-                        </FieldSelect>
-                    </Grid>
-                </Paragraph>
-                <Paragraph>
-                    <Grid>
-                        <span> Kwota: </span>
-                        <Field as="input"
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                            type="number"
-                            name="amountToConvert"
-                            required
-                            min="1"
-                            step="any"
-                            placeholder="1"
-                            autoFocus
-                        />
-                    </Grid>
-                </Paragraph>
-                <Paragraph>
-                    <Button>Przelicz</Button>
-                </Paragraph>
-                <Paragraph>
-                    <RatesUpdatedTime ratesData={ratesData} />
-                </Paragraph>
-            </Fieldset>
-            <Result result={result} />
-        </StyledForm>
+        ratesData.status === "loading" ? (
+            <LoadingStatus>
+                Zaczekaj chwilę.<br /> Ładuję aktualne kursy walut...
+                <LoadingAnimation />
+            </LoadingStatus>)
+            : ratesData.status === "error" ? (
+                <ErrorStatus>
+                    Coś poszło nie tak!
+                </ErrorStatus>)
+                : (
+                    <StyledForm onSubmit={onSubmit}>
+                        <Fieldset>
+                            <Legend>Zamień na złotówki:</Legend>
+                            <Paragraph>
+                                <Grid>
+                                    <span> Waluta: </span>
+                                    <FieldSelect
+                                        onChange={({ target }) => setCurrency(target.value)}
+                                        name="convertFrom"
+                                        value={currency}
+                                    >
+                                        {!!ratesData.rates && Object.keys(ratesData.rates).map(currency => (
+                                            <option key={currency} value={currency}>
+                                                {currency}
+                                            </option>
+                                        ))}
+                                    </FieldSelect>
+                                </Grid>
+                            </Paragraph>
+                            <Paragraph>
+                                <Grid>
+                                    <span> Kwota: </span>
+                                    <Field as="input"
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                        type="number"
+                                        name="amountToConvert"
+                                        required
+                                        min="1"
+                                        step="any"
+                                        placeholder="1"
+                                        autoFocus
+                                    />
+                                </Grid>
+                            </Paragraph>
+                            <Paragraph>
+                                <Button>Przelicz</Button>
+                            </Paragraph>
+                            <Paragraph>
+                                <RatesUpdatedTime ratesData={ratesData} />
+                            </Paragraph>
+                        </Fieldset>
+                        <Result result={result} />
+                    </StyledForm>
+                )
     );
 };
 
